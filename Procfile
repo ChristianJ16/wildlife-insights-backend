6e9a3a3e87dd2dfbb97253eedac33d9365444ce0
christianj16 <christianjansen16@gmail.com>
2024-07-05 23:40:56 -0700

    getting ready for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wildlife_insights.wsgi
--- wildlife_insights/settings.py
+++ wildlife_insights/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'wildlife_insights.urls'
 
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
